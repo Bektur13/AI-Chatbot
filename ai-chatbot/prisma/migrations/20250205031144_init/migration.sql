a84212f15a02fc71ee6787615d345ca1091b8494
Bektur13 <duyshembekov04@mail.ru>
2025-02-04 20:26:45 -0800

    Completed doing schemas in Prisma

---
+++ ai-chatbot/prisma/migrations/20250205031144_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "stripeId" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
